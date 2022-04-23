fa89f94b1902050689253422b26fa3e4dda2fb05
snanoh <njs1324@gmail.com>
2022-04-23 17:02:17 +0900

    4.23 HelloTraceV2 생성

--- src/main/java/hello/advanced/trace/TraceStatus.java
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -1,5 +1,6 @@
 package hello.advanced.trace;
 
+//로그 상태 정보를 낱타내는 클래스
 public class TraceStatus {
 
     private TraceId traceId;
--- src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class HelloTraceV1 {
+public class HelloTraceV2 {
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
@@ -19,6 +19,14 @@ public class HelloTraceV1 {
         //로그 출력
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    public TraceStatus beginSync(TraceId beforeTraceId, String message){
+        TraceId nextId = beforeTraceId.createNextId();
+        long startTimeMs = System.currentTimeMillis();
+        //로그 출력
+        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX, nextId.getLevel()), message);
+        return new TraceStatus(nextId, startTimeMs, message);
     }
 
     public void end(TraceStatus status) {
